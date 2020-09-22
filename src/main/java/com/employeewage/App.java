fea2a073a20f2f4cb8e96d1c71b45d36c26199ad
Akhil-star <akhilakki1305@gmail.com>
2020-09-22 13:41:17 +0530

    Added:Dailywage

--- src/main/java/com/employeewage/App.java
+++ src/main/java/com/employeewage/App.java
@@ -9,16 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welocome to Employee Wage Computation Program" );
-        int employeePresent = 1;
-		
-    	int value = ((int) Math.random() * 10)%2;
-    			
-    	System.out.println("Random value : "+value);
-    			
-    	if(value == 1) {
-    	System.out.println("Employee present");
-    	} else {
-    	System.out.println("Employee absent");
-    			}
+        calDailywage();
+    }
+    public static void calDailywage() {
+    	int wage;
+    	wage=20*8;
+    	System.out.println(wage);
     }
 }

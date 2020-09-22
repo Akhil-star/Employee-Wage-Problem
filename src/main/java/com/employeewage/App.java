313206f1f550ca68f9c9ae93e164f35f48197ce5
Akhil <64737697+Akhil-star@users.noreply.github.com>
2020-09-22 13:46:00 +0530

    Update App.java

--- src/main/java/com/employeewage/App.java
+++ src/main/java/com/employeewage/App.java
@@ -9,16 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welocome to Employee Wage Computation Program" );
-        int employeePresent = 1;
-		
-    	int value = ((int) Math.random() * 10)%2;
-    			
-    	System.out.println("Random value : "+value);
-    			
-    	if(value == 1) {
-    	System.out.println("Employee present");
-    	} else {
-    	System.out.println("Employee absent");
-    			}
     }
 }
